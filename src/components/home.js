ef534302093b7cd262d1faf0547480966c6d9362
Enrique <enrangel@csumb.edu>
2024-12-15 18:42:19 -0800

    added the links and pages

--- src/components/home.js
+++ src/components/home.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useNavigate } from 'react-router-dom'; // Import useNavigate for navigation
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './navbar';
+import './home.css'; // Import custom CSS
 
 function Home() {
   const navigate = useNavigate(); // Hook for navigation
@@ -10,14 +11,13 @@ function Home() {
     <div className="d-flex flex-column min-vh-100">
       <Navbar />
       <div className="container text-center mt-5 flex-grow-1">
-        <h1>Home Page</h1>
-        <p>Page content.</p>
+        <h1>Home</h1>
+        {/* <p>Page content.</p> */}
 
-        <div className="row row-cols-1 row-cols-md-4 g-4">
-          {/* Food Card */}
-          <div className="col">
+        <div className="row justify-content-center">
+          <div className="col-md-4 mb-4">
             <div className="card" onClick={() => navigate('/food')}>
-              <img src="https://via.placeholder.com/150" className="card-img-top" alt="Food" />
+              <img src="https://img.freepik.com/premium-vector/food-donation-meal-donates-vector-illustration_353502-460.jpg" className="card-img-top" alt="Food" />
               <div className="card-body">
                 <h5 className="card-title">Food</h5>
                 <p className="card-text">Scholarships, grants, etc.</p>
@@ -25,10 +25,9 @@ function Home() {
             </div>
           </div>
 
-          {/* Basic Needs Card */}
-          <div className="col">
+          <div className="col-md-4 mb-4">
             <div className="card" onClick={() => navigate('/basic')}>
-              <img src="https://via.placeholder.com/150" className="card-img-top" alt="Basic Needs" />
+              <img src="https://www.shutterstock.com/image-vector/basic-needs-vector-icon-need-600nw-2302590675.jpg" className="card-img-top" alt="Basic Needs" />
               <div className="card-body">
                 <h5 className="card-title">Basic Needs</h5>
                 <p className="card-text">Cal Fresh, county food bank, etc</p>
@@ -36,10 +35,9 @@ function Home() {
             </div>
           </div>
 
-          {/* Education Card */}
-          <div className="col">
+          <div className="col-md-4 mb-4">
             <div className="card" onClick={() => navigate('/education')}>
-              <img src="https://via.placeholder.com/150" className="card-img-top" alt="Education" />
+              <img src="https://www.cft.org/sites/main/files/imagecache/thumbnail/main-images/scholarship-application-portal_is-1220073028_0.png?1637609139" className="card-img-top" alt="Education" />
               <div className="card-body">
                 <h5 className="card-title">Education</h5>
                 <p className="card-text">Description</p>
--- src/components/navbar.js
+++ src/components/navbar.js
@@ -6,15 +6,13 @@ function Navbar() {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
       <div className="container-fluid">
-        {/* Left-aligned Home link */}
+
         <Link className="navbar-brand" to="/">Home</Link>
 
-        {/* Toggle button for mobile view */}
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/* Center-aligned links */}
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav mx-auto">
             <li className="nav-item">
